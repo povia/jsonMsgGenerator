d56f78da6ff1902ae4fae5801f174a18bcc95836
povia <47127607+povia@users.noreply.github.com>
2020-07-01 01:04:11 +0800

    adding OperatingSystemMXBean

--- src/main/java/com/tistory/povia/jsontest/msg/JsonGenerator.java
+++ src/main/java/com/tistory/povia/jsontest/msg/JsonGenerator.java
@@ -1,10 +1,13 @@
 package com.tistory.povia.jsontest.msg;
 
-import java.io.File;
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
+import java.lang.management.RuntimeMXBean;
 import java.time.LocalDateTime;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class JsonGenerator {
@@ -14,10 +17,11 @@ public class JsonGenerator {
 		
 		TimerTask task = new TimerTask() {
 			int i=0;
-			String nameOs = "os.name";
-			String versionOs = "os.version";
-			String archiOs = "os.arch";
-			
+//			String nameOs = "os.name";
+//			String versionOs = "os.version";
+//			String archiOs = "os.arch";
+			OperatingSystemMXBean bean = (OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean();
+			RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
 			@Override
 			public void run() {
 //				File[] roots = File.listRoots();
@@ -25,13 +29,31 @@ public class JsonGenerator {
 				
 				
 				JSONObject object = new JSONObject();
+				JSONArray arr = new JSONArray();
+				bean.getAvailableProcessors();
 				object.put("Date", LocalDateTime.now());
-				object.put("사용 가능 프로세서 수", Runtime.getRuntime().availableProcessors());
-				object.put("남은 메모리", Runtime.getRuntime().freeMemory());
-				object.put("message", (++i)+"번째 메시지입니다.");
+				object.put("Os Name", bean.getName());
+				object.put("Os Version", bean.getVersion());
+				object.put("Avg System Load", bean.getSystemLoadAverage());
+				object.put("Available Processers", bean.getAvailableProcessors());
+				object.put("System Architecture", bean.getArch());
+				object.put("Total Physical Memory", bean.getTotalPhysicalMemorySize());
+				object.put("Free Physical Memory", bean.getFreePhysicalMemorySize());
+				object.put("System CPU Load", bean.getSystemCpuLoad());
+				arr.add(object);
 				
+				object = new JSONObject();
+				object.put("Process CPU Load", bean.getProcessCpuLoad());
+				object.put("Process CPU Time",bean.getProcessCpuTime());
+//				object.put("Process Name",runtimeBean.getName());
+//				object.put("Management Spec Version",runtimeBean.getManagementSpecVersion());
+//				object.put("JVM Vendor", runtimeBean.getVmVendor());
+//				object.put("Library Path", runtimeBean.getLibraryPath());
+//				object.put("System Property", runtimeBean.getSystemProperties());
 				
-				System.out.println(object.toJSONString());
+				arr.add(object);
+				
+				System.out.println(arr.toJSONString());
 			}
 		};
 		
